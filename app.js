4e5e51043ea240a756dbdc4b9ec03d8be4c3c70b
Kenneth Quintero <kennethgqv@gmail.com>
2021-05-09 14:03:19 -0600

    this storage the buttons nodes in js

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
+// getting the nodes
+const screen = document.querySelector('.calculator__screen');
+const clearBtn = document.querySelector('.calculator__clear');
+
+const divideBtn = document.querySelector('.calculator__divide');
+const multiplyBtn = document.querySelector('.calculator__multiply');
+const substractBtn = document.querySelector('.calculator__substract')
+const addBtn = document.querySelector('.calculator__add')
+
+const equalBtn = document.querySelector('.calculator__equal');
+const dotBtn = document.querySelector('.calculator__dot');
+
+// all the number buttons are here | numberBtn[1] = 1, numberBtn[2] = 2 ...
+const numberBtn = document.querySelectorAll('.number');
+console.table(numberBtn);
+
+// main functions
 function add(numArray) {
     const total = numArray.reduce((total, num) => {
         return total += num;
@@ -29,3 +46,4 @@ function divide(numArray) {
 function operate(a, b, operation) {
     return operation([a, b]);
 }
+
