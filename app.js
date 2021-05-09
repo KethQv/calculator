32aec55edec76758265bbed417c5d7c763845e7c
Kenneth Quintero <kennethgqv@gmail.com>
2021-05-08 21:58:38 -0600

    4 principal functions and fix README

---
+++ app.js
@@ -0,0 +1,27 @@
+function add(numArray) {
+    const total = numArray.reduce((total, num) => {
+        return total += num;
+    }, 0);
+    return total;
+}
+
+function substract(numArray) {
+    const total = numArray.reduce((total, num) => {
+        return total -= num;
+    }, numArray[0] * 2);
+    return total;
+}
+
+function multiply(numArray) {
+    const total = numArray.reduce((total, num) => {
+        return total *= num;
+    }, 1);
+    return total;
+}
+
+function divide(numArray) {
+    const total = numArray.reduce((total, num) => {
+        return total /= num;
+    }, numArray[0] * numArray[0]);
+    return total;
+}
