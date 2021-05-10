936ae74d608524c8ae94a85ba5df2b44bc8c505c
Kenneth Quintero <kennethgqv@gmail.com>
2021-05-10 01:00:23 -0600

    this add a functional calculator and info to README

--- app.js
+++ app.js
@@ -15,26 +15,25 @@ const clearBtn = document.querySelector('.calculator__clear');
 const equalBtn = document.querySelector('.calculator__equal');
 const dotBtn = document.querySelector('.calculator__dot');
 
-const operations = [
-    { name: 'divide', symbol: '/', function: divide, button: document.querySelector('.calculator__divide') },
-    { name: 'multiply', symbol: '*', function: multiply, button: document.querySelector('.calculator__multiply') },
-    { name: 'substract', symbol: '-', function: substract, button: document.querySelector('.calculator__substract') },
-    { name: 'add', symbol: '+', function: add, button: document.querySelector('.calculator__add') }
-];
-
-// all the number buttons are here | numberBtn[1] = 1, numberBtn[2] = 2 ...
 const numberBtn = document.querySelectorAll('.number-button');
 
+const operations = [
+    { symbol: '/', function: divide, button: document.querySelector('.calculator__divide') },
+    { symbol: '*', function: multiply, button: document.querySelector('.calculator__multiply') },
+    { symbol: '-', function: substract, button: document.querySelector('.calculator__substract') },
+    { symbol: '+', function: add, button: document.querySelector('.calculator__add') }
+];
+
 // main functions
-function divide(numArray) {
+function divide(a, b) {
     return a / b;
 }
 
-function multiply(numArray) {
+function multiply(a, b) {
     return a * b;
 }
 
-function substract(numArray) {
+function substract(a, b) {
     return a - b;
 }
 
@@ -46,13 +45,6 @@ function operate(a, b, operation) {
     return operation(a, b);
 }
 
-function evaluateOperation() {
-    operationArray = operationString.split(' ');
-
-    // here is the main logic of the calculator
-
-}
-
 function displayOperation(content) {
     operationString += content;
     screenOperation.textContent = operationString;
@@ -60,8 +52,63 @@ function displayOperation(content) {
     // evaluateOperation();
 }
 
-function displayResult() {
-    screenResult.textContent = operationResult;
+function displayResult(result) {
+    screenResult.textContent = result;
+}
+
+function evaluateOperation() {
+
+    let isThereMultiOrDiv = operationArray.some(digit => digit == '/' || digit == '*');
+    let isThereAddOrSubs = operationArray.some(digit => digit == '-' || digit == '+');
+
+    if (isThereMultiOrDiv) { 
+
+        let indexOfMultiOrDiv = operationArray.findIndex(digit => digit == '/' || digit == '*');
+
+        var a = parseFloat(operationArray[indexOfMultiOrDiv - 1]);
+        var b = parseFloat(operationArray[indexOfMultiOrDiv + 1]);
+
+        if (b != undefined || b != null || !isNaN(b)) {
+            let result;
+
+            if (operationArray[indexOfMultiOrDiv] == '/') {
+                // call divide
+                result = operate(a, b, divide);
+            } else {
+                // call multiply
+                result = operate(a, b, multiply);
+            }
+            operationArray.splice(indexOfMultiOrDiv - 1, 3, result);
+        } 
+
+    } else if (isThereAddOrSubs) { 
+
+        let indexOfAddOrSubs = operationArray.findIndex(digit => digit == '-' || digit == '+');
+
+        var a = parseFloat(operationArray[indexOfAddOrSubs - 1]);
+        var b = parseFloat(operationArray[indexOfAddOrSubs + 1]);
+
+        if (b != undefined || b != null || !isNaN(b)) {
+            let result;
+
+            if (operationArray[indexOfAddOrSubs] == '-') {
+                // call substract
+                result = operate(a, b, substract);
+            } else {
+                // call add
+                result = operate(a, b, add);
+            }
+            operationArray.splice(indexOfAddOrSubs - 1, 3, result);
+        }
+
+    }
+
+    if (operationArray.length == 1) {
+        displayResult(operationArray[0]);
+        return;
+    }
+
+    evaluateOperation();
 }
 
 // event listeners
@@ -79,12 +126,16 @@ dotBtn.onclick = () => {
     }
 };
 
+equalBtn.onclick = () => {
+    operationArray = operationString.split(' ');
+    evaluateOperation();
+};
+
 clearBtn.onclick = () => {
     operationString = '';
     displayOperation(operationString);
 
     operationArray = [];
-    // evaluateOperation();
     displayResult();
 
     operationBtnAlreadyPressed = false;
@@ -100,36 +151,3 @@ operations.map((operation) => {
         } 
     });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const divideBtn = document.querySelector('.calculator__divide');
-// const multiplyBtn = document.querySelector('.calculator__multiply');
-// const substractBtn = document.querySelector('.calculator__substract');
-// const addBtn = document.querySelector('.calculator__add');
